a7d7234606d48632115db3368fe1c5d13c1ab8e5
AkshaySalamwade <akshaysalamwade@gmail.com>
2024-03-31 21:21:27 +0530

    Game UI updated

--- cricketJs.js
+++ cricketJs.js
@@ -7,20 +7,19 @@ let currentScore = 0;
 let playerWin = function () {
   if (totalScorePlayer1 > totalScorePlayer2) {
     console.log(`Player 1 Wins !!`);
-    document.getElementById('circleContainer').textContent = 'Player 1 Wins !!';
+    document.getElementById("circleContainer").textContent = "Player 1 Wins !!";
     console.log(`Click on 'New Match' Button to start the New Match..`);
   } else if (totalScorePlayer1 == totalScorePlayer2) {
     console.log(`Match is Tied !!`);
-    document.getElementById('circleContainer').textContent = 'Match is Tied !!';
+    document.getElementById("circleContainer").textContent = "Match is Tied !!";
   } else {
     console.log(`Player 2 Wins !!`);
-    document.getElementById('circleContainer').textContent = 'Player 2 Wins!!';
+    document.getElementById("circleContainer").textContent = "Player 2 Wins!!";
     console.log(`Click on 'New Match' Button to start the New Match..`);
   }
 };
 
 document.querySelector("#playBtn1").addEventListener("click", function () {
-
   //alert("function Called");
   let bookSize = document.querySelector("#bookSize").value;
   let totalWickets = document.querySelector("#totalWickets").value;
@@ -33,33 +32,35 @@ document.querySelector("#playBtn1").addEventListener("click", function () {
   let playerTurn1 = function () {
     if (currentPlayerScore === 0) {
       playerWickets++;
-      if (playerWickets < totalWickets) {debugger;
+      if (playerWickets < totalWickets) {
+        debugger;
         console.log(`Out !! ${playerWickets} wickets down !! `);
-        document.getElementById('circleContainer').textContent = 'OUT';
+        document.getElementById("circleContainer").textContent = "OUT";
 
         console.log(`------------------------------------------------`);
       } else {
         totalScorePlayer1 = totalScore;
         console.log(
-          `You are all Out & Total Runs scored : ${totalScorePlayer1}`   
+          `You are all Out & Total Runs scored : ${totalScorePlayer1}`
         );
-        document.getElementById('circleContainer').textContent = 'ALL OUT';
+        document.getElementById("circleContainer").textContent = "ALL OUT";
         console.log(`------------------------------------------------`);
         totalScore = 0;
         playerWickets = 0;
         document.querySelector("#playBtn1").disabled = true;
       }
-    } else {debugger;
+    } else {
+      debugger;
       totalScore += currentPlayerScore;
       console.log(`Page Flipped  > ${currentPlayerScore} runs !`);
-    
-     // document.getElementById('CurrentRun').innerHTML = currentPlayerScore;
-      document.getElementById('circleContainer').textContent = currentPlayerScore;
+
+      // document.getElementById('CurrentRun').innerHTML = currentPlayerScore;
+      document.getElementById("circleContainer").textContent =
+        currentPlayerScore;
       console.log(`Total Runs > ${totalScore} runs`);
-      document.getElementById('totalScore1').innerHTML = totalScore;
+      document.getElementById("totalScore1").innerHTML = totalScore;
       console.log(`------------------------------------------------`);
     }
-
   };
   playerTurn1();
 });
@@ -78,14 +79,14 @@ document.querySelector("#playBtn2").addEventListener("click", function () {
       playerWickets++;
       if (playerWickets < totalWickets) {
         console.log(`Out !! ${playerWickets} wickets down !! `);
-        document.getElementById('circleContainer').textContent = 'OUT';
+        document.getElementById("circleContainer").textContent = "OUT";
         console.log(`------------------------------------------------`);
       } else {
         totalScorePlayer2 = totalScore;
         console.log(
           `You are all Out & Total Runs scored : ${totalScorePlayer2}`
         );
-        document.getElementById('circleContainer').textContent = 'ALL OUT';
+        document.getElementById("circleContainer").textContent = "ALL OUT";
         console.log(`------------------------------------------------`);
         totalScore = 0;
         playerWickets = 0;
@@ -99,8 +100,9 @@ document.querySelector("#playBtn2").addEventListener("click", function () {
       } else {
         totalScore += currentPlayerScore;
         console.log(`Page Flipped  > ${currentPlayerScore} runs !`);
-        document.getElementById('circleContainer').textContent = currentPlayerScore;   
-        document.getElementById('totalScore2').innerHTML = totalScore;
+        document.getElementById("circleContainer").textContent =
+          currentPlayerScore;
+        document.getElementById("totalScore2").innerHTML = totalScore;
         console.log(`Total Runs > ${totalScore} runs`);
         console.log(`------------------------------------------------`);
       }
@@ -111,6 +113,7 @@ document.querySelector("#playBtn2").addEventListener("click", function () {
 });
 
 document.querySelector("#newMatchBtn").addEventListener("click", function () {
+  //alert("Called");
   console.clear();
   console.log(`New Match is Ready to Begin !! `);
   document.querySelector("#playBtn1").disabled = false;
@@ -119,4 +122,11 @@ document.querySelector("#newMatchBtn").addEventListener("click", function () {
   totalScorePlayer1 = 0;
   totalScorePlayer2 = 0;
   playerWickets = 0;
+
+  document.getElementById("tPlayer").innerHTML = 0;
+  document.getElementById("bSize").innerHTML = 0;
+  document.getElementById("tPlayer1").innerHTML = 0;
+  document.getElementById("bSize1").innerHTML = 0;
+  document.getElementById("totalScore1").innerHTML = totalScore;
+  document.getElementById("totalScore2").innerHTML = totalScore;
 });
